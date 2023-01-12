bc52d9f5da83a7512e5524376c12b2e6a9001aa1
HeroRemodeling <122572734+HeroRemodeling@users.noreply.github.com>
2023-01-12 14:39:01 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,10 @@ export default function Home() {
           Get free estimate today, 
     get free design today for indoor or outdoor project, 
     get 100% finance with energy efficient products Call today (818) 470-2749 Simon or (818) 859-5873 Yaniv.
-  Best in the west no sub contractors a family business from A to Z a top quality for your needs .
+  Best in the west no sub contractors a family business with 20 years experience.
         </p>
   <p className="details">
-          I am almost 20 years at the remodeling construction business, 
-            my family most of them born to renovation industry, 
-              my mantra is always clinet right and make a clinet happy is my main goal.
+              My mantra is always client right and make a clieny happy is my main goal.
 Our company will take you from a-z buying all materials needed and provide blue prints design and ideas to make your home a dream. 
 I am always available 364 days a year try us and feel free if any questions contact me directly.
         </p>
